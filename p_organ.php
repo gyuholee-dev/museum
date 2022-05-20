05501fb807993e363ffa012cfb088b442b5983a5
noel1967 <noel1967@naver.com>
2022-05-20 12:06:48 +0900

    조직도페이지

---
+++ p_organ.php
@@ -0,0 +1,298 @@
+<?php
+   session_start();
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>포항시립미술관</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=2.0,minimum-scale=1.0">
+<link rel="icon" href="images/slogo.png">
+<link rel="stylesheet" href="styles/style.css">
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="scripts/script.js" defer="defer"></script>
+</head>
+
+<body>
+<div id="container" class="sub">
+	<!-- sCon -->
+	<div id="sCon">
+		<!-- lnb -->
+		<div id="lnb">
+			<h2>미술관소개</h2>
+			<ul>
+				<li class=""><a href="/poma/p_intr/i_greeting/">인사말</a></li>
+				<li class=""><a href="/poma/p_intr/vision/">정체성·미션</a></li>
+				<li class=""><a href="/poma/p_intr/i_intr/">시설현황</a></li>
+				<li class="active"><a href="/poma/p_intr/i_organization/">조직도</a></li>
+				<li class=""><a href="/poma/p_intr/i_mi/">MI소개</a></li>
+			</ul>
+		</div>
+		<!-- lnb -->
+
+		<!-- contents -->
+		<div class="contents">
+			<!-- sub_visual -->
+			<div class="sub_visual sub07"></div>
+			<!-- sub_visual -->
+
+			<!-- cont -->
+			<div id="cont">
+				<div class="location">
+					<span class="home">HOME</span>
+					<span class="loc1 arrow">미술관소개</span>
+					<span class="loc2 arrow">조직도</span>
+				</div>
+				<div class="sub_tit">
+					<h3 id="CI3f48189f60880936bdcf825731ed1023">조직도 <span class="eng">Organization chart</span></h3>
+				</div>
+
+				<!-- inner -->
+				<div class="inner">
+                                <div class="img_wrap organization_wrap">
+					<img src="images/organization_img01.png" alt="관장 김갑수, 운영팀,학예연구팀, 운영위원회, 작품평가위원회">
+				</div>
+		<div class="text_wrap">
+			<h3 class="cont_tit">포항시립미술관
+			<p class="director">대표전화 : 054-270-4700  |   <span class="emph_LGray">팩스 : 054-252-0257</span></p>
+			</h3>
+<!--			<p class="txt1">1. 운영담당</p>  -->
+
+			<div class="brdList_wrap scroll">
+				<table class="brdList">
+					<colgroup>
+						<col style="width:13%;">
+						<col style="width:15%;">
+						<col style="width:72%;">
+					</colgroup>
+					<thead>
+						<tr>
+							<th class="noLLine">직위</th>
+							<th>성명</th>
+							<th>담당업무</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="noLLine">관장</td>
+							<td>김갑수</td>
+							<td class="alignL">						
+								<ul class="bulDotList">
+									<li>· 시립미술관 업무 총괄</li>
+								</ul>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
+		<div class="text_wrap">
+			<h3 class="cont_tit">운영팀</h3>
+			<div class="brdList_wrap scroll">
+			<table class="brdList">
+				<colgroup>
+					<col style="width:13%;">
+					<col style="width:15%;">
+					<col style="width:45%;">
+					<col style="width:27%;">
+				</colgroup>
+				<thead>
+					<tr>
+						<th class="noLLine">직위</th>
+						<th>성명</th>
+						<th>담당업무</th>
+						<th>연락처</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class="noLLine">운영팀장</td>
+						<td>고연경</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 미술관행정의 종합 및 기본운영계획 수립</li>
+								<li>· 인사·복무사항 전반</li>
+								<li>· 의회, 조례 제개정 관련업무</li>
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4633</p>
+							<p>E-mail :  kykje@korea.kr </p>
+						</td>
+					</tr>
+					<tr>
+						<td class="noLLine">행정</td>
+						<td>류희정</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 주요업무, 의회 관련 업무</li>
+								<li>· 미술관(재난물품포함) 물품 및 재산관리</li>
+								<li>· 일반서무, 예산 및 회계업무</li>
+								<li>· 기간제근로자 및 공공근로 관리</li>
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4635</p>
+							<p>E-mail :  jkhui4217@korea.kr </p>
+						</td>
+					</tr>
+					<tr>
+						<td class="noLLine">시설관리</td>
+						<td>서강진</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 미술관 시설관리 전반</li>
+								<li>· 미술관 기타시설 대관업무</li>
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4634</p>
+							<p>E-mail :  skjolo@korea.kr</p>
+						</td>
+					</tr>
+
+				</tbody>
+			</table>
+		</div>
+		</div>
+
+		<div class="text_wrap">
+			<h3 class="cont_tit">학예연구팀	</h3>
+			<div class="brdList_wrap scroll">
+			<table class="brdList">
+				<colgroup>
+					<col style="width:13%;">
+					<col style="width:15%;">
+					<col style="width:45%;">
+					<col style="width:27%;">
+				</colgroup>
+				<thead>
+					<tr>
+						<th class="noLLine">직위</th>
+						<th>성명</th>
+						<th>담당업무</th>
+						<th>연락처</th>
+					</tr>
+				</thead>
+				<tbody>
+
+					<tr>
+						<td class="noLLine">학예연구팀장</td>
+						<td>이보경</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 학예연구팀 업무 총괄</li>
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4702</p>
+							<p>E-mail :  itsbk2013@korea.kr </p>
+						</td>
+					</tr>
+					<tr>
+						<td class="noLLine">행정</td>
+						<td>이경남</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 학예 일반 서무</li>
+								<li>· 학예업무 관련 지출</li>
+ 								<li>· 전시실 안내 용역 및 관람객수 관리</li>
+								<li>· 스틸아트공방 지출 업무</li>
+								<li>· 홈페이지 및 관람객수 프로그램 관리</li>
+								<li>· 도서자료실 관리</li>
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4703</p>
+							<p>E-mail :  leekn617@korea.kr</p>
+						</td>
+					</tr>
+					<tr>
+					    <td class="noLLine" rowspan="3">학예사</td>
+						<td>김한아</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 연간 홍보 계획 수립 및 추진</li>
+								<li>· 전시 기획 및 추진  </li>
+								<li>· 스마트 야외소장품 해설시스템 운영 </li>
+                                <li>· 미술관 대외교류 및 협력 </li>
+                                <!-- <li>· 인턴 및 도슨트 프로그램 운영</li> -->
+                                <li>· 미술관 운영위원회 운영</li>
+								<li>· 미술관 연보제작 </li>
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4704</p>
+							<p>E-mail :  kha0711@korea.kr </p>
+						</td>
+					</tr>
+					<tr>
+						<td>김주란</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 스틸아트공방 운영 계획 수립 및 추진 </li>
+								<li>· 전시 기획 및 추진</li>
+								<li>· 장두건미술상 운영위원회 및 미술상 운영</li>
+								<li>· 홈페이지 콘텐츠 관리</li>
+                                <li>· 포항스틸아트페스티벌 지원</li>
+                                <!-- <li>· 미술관 자원봉사 관리</li> -->
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4636</p>
+							<p>E-mail : ran1030@korea.kr</p>
+						</td>
+					</tr>
+
+					<tr>
+						<td>최서영</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 소장품 수집, 보존‧관리 계획 수립 및 추진</li>
+								<!-- <li>· 소장품 및 아카이브 수집 및 작품평가위원회 운영</li> -->
+								<li>· 전시 기획 및 추진</li>
+								<li>· 작품수집심의위원회 및 작품가격평가위원회 운영</li>
+                                                                <li>지역미술사 연구용역</li>
+								<li>· 전시연계 학술 프로그램 추진</li>
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4705</p>
+							<p>E-mail : choiseoyoung@korea.kr </p>
+						</td>
+					</tr>
+
+					
+					
+					<tr>
+						<td class="noLLine">문화예술교육사</td>				
+						<td>최혜지</td>
+						<td class="alignL">
+							<ul class="bulDotList">
+								<li>· 연간 교육프로그램 계획 수립 및 추진</li>
+								<!-- <li>· 전시연계 학술 프로그램 추진</li> -->
+								<li>· 인턴 및 도슨트 프로그램 운영</li>
+								<li>· Poma 아카데미 운영</li>
+								<li>· 미술관 음악회 운영</li>
+								<li>· 미술관 자원봉사자 관리</li>
+							</ul>
+						</td>
+						<td class="alignL">
+							<p>Tel : 054-270-4706</p>
+							<p>E-mail :  pmed2703@korea.kr </p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		</div>
+
+		</div>
+		</div>
+		</div>
+
+	</div>
+	<!-- //contents -->
+</div>
+</body></html>
