8cd436c20a4abdf43ce0ef31733a6cd2419b4d2f
noel1967 <noel1967@naver.com>
2022-05-20 11:48:33 +0900

    MI소개페이지

---
+++ p_intr.php
@@ -0,0 +1,94 @@
+<?php
+   session_start();
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>포항시립미술관</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=2.0,minimum-scale=1.0">
+<link rel="icon" href="images/slogo.png">
+<link rel="stylesheet" href="styles/style.css">
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="scripts/script.js" defer="defer"></script>
+</head>
+
+<body>
+
+<div id="container" class="sub">
+	<!-- sCon -->
+	<div id="sCon">
+		<!-- lnb -->
+		<div id="lnb">
+			<h2>미술관소개</h2>
+			<ul>
+				<li class=""><a href="/poma/p_intr/i_greeting/">인사말</a></li>
+				<li class=""><a href="/poma/p_intr/vision/">정체성·미션</a></li>
+				<li class=""><a href="/poma/p_intr/i_intr/">시설현황</a></li>
+				<li class=""><a href="/poma/p_intr/i_organization/">조직도</a></li>
+				<li class="active"><a href="/poma/p_intr/i_mi/">MI소개</a></li>
+			</ul>
+		</div>
+		<!-- lnb -->
+
+		<!-- contents -->
+		<div class="contents">
+			<!-- sub_visual -->
+			<div class="sub_visual sub07"></div>
+			<!-- sub_visual -->
+
+			<!-- cont -->
+			<div id="cont">
+				<div class="location">
+					<span class="home">HOME</span>
+					<span class="loc1 arrow">미술관소개</span>
+					<span class="loc2 arrow">MI소개</span>
+				</div>
+				<div class="sub_tit">
+					<h3 id="CI7c8b24786c031860f1a36598fb17cf12">MI소개 <span class="eng">MI Introduce</span></h3>
+				</div>
+
+				<!-- inner -->
+				<div class="inner">
+
+
+
+		<div class="mi_wrap clearAfter">
+		<!--<h3 class="visual_text">
+			자연을 산책하는 숲속의미술관
+			<span class="stxt">시민이 감동하는, 작지만 차별화된, 세계적인 미술관으로 자리매김하겠습니다. </span>
+		</h3>-->
+			<div class="img">
+				<img src="images/mi_img01.jpg" alt="">
+			</div>
+			<div class="text">
+				<p class="tit">Museum Identity</p>
+				<div class="cont marB10">
+					포항시립미술관 MI는 영문 “Pohang Museum Of Steel Art”의 이니셜 “P.O.M.A”를 모던한 형태로 디자인 한 것으로 해와 달의 정령인 “연오랑 세오녀”의 일월 신화와 한반도의 아침을 여는 영일만의 일출을 시각화 하였습니다.
+				</div>
+				<p class="tit">Museum Concept</p>
+				<div class="cont">
+					<ul class="bulDotList">
+						<li>· 연오랑 세오녀의 일월신화</li>
+						<li>· 영일만의 일출</li>
+						<li>· 태양</li>
+						<li>· 가독성</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="mi_wrap">
+			<img src="images/mi_img02.jpg" alt="" class="pc_ver">
+		</div>
+			</div>
+			</div>
+			</div>
+	</div>
+	<!-- //contents -->
+
+</div>
+
+
+
+</body></html>
