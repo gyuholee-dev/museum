526611f52836ddc3d346770c33abdcfc9124550e
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-20 16:19:21 +0900

    임시

--- test.php
+++ test.php
@@ -1,67 +1,60 @@
 <?php
 $str = <<<HTML
-<div class="boxi">
-    <h2 class="fictionbox">메타픽션 : 현실 그 너머</h2>
+<div class="text_wrap">
+    <h3 class="cont_tit">정체성</h3>
+    <div class="identity_wrap">
+        <div class="img"><img src="images/identity_img01.jpg" alt=""></div>
+        <div class="txt">
+            <div class="inner">
+                <p class="txt1">포항시립미술관은<br>세계 유일의 스틸아트 미술관입니다.</p>
+                <p class="txt2">지역의 역사적·문화적 정체성은 ‘철’이라는 재료 속에 <br>상징적으로 녹아 있습니다.</p>
+                <p class="txt2">철의 탄생이 인류 문명사의 급진적 전환을 이끌었듯이 우리<br> 미술관은 스틸아트를 통해 ‘삶 속에서 예술이, 예술 속에 삶’이<br>호흡하는 신철기시대(neo-iron age)를 열어가고자 합니다.</p>
+            </div>
+        </div>
+    </div>
+</div>
 
-    <p><b>전시기간</b>    <span style="color:gray;">2022-01-25 ~ 2022-05-08</span></p>
-    <p><b>전시장소</b>    <span style="color:gray;">1, 2 전시실</span></p>
-    <p><b>전시작품</b>    <span style="color:gray;">회화 및 설치 등 50여점</span></p>
-    <p><b>참여작가</b>    <span style="color:gray;">마르크 샤갈<b>(Marc Chagall, 1887-1985),</b></span></p>
-    <p><span style="color:gray;">            호안 미로<b>(Joan Miro, 1893-1983),</span></p></b>
-    <p><span style="color:gray;">            살바도르 달리<b>(Salvador Dali, 1904-1989),</span></b></p>
-    <p><span style="color:gray;">            쿠사마 야요이<b>(Kusama Yayoi, b.1929),</span></b></p> 
-    <p><span style="color:gray;">            변종곤<b>(BYUN Chonggon, b.1948),</b> 이미주<b>(LEE Miju, b.1982),</span></b></p>
-    <p><span style="color:gray;">            김미진<b>(KIM Mijin, b.1979),</span></b></p>
-    <p><span style="color:gray;">            쑨지<b>(Soon Ji, b.1983),</span></b></p>
-    <p><span style="color:gray;">            이병찬<b>(LEE Byungchan, b.1987)</span></b></p>
 
-    <p><b>관람시간</b>   <span style="color:gray;">하절기(4-10월) : <b>오전10시 - 오후7시</span></p></b>
-    <p><span style="color:gray;">           동절기(11-3월) :<b>오전10시 - 오후6시</span></b></p>
-    <p><span style="color:gray;">           <b>*월요일 휴관*</span></b></p>
-    <p><b>관람료</b>    <span style="color:gray;"><b>*무료*</b></span></p>
-</div>    
+<div class="text_wrap">
+    <h3 class="cont_tit">미션</h3>
+    <div class="mission_wrap alignC bdTop">
+        <img src="images/mission_img01.png" alt="">
+    </div>
+</div>
 
-<pre><p>
-《메타픽션: 현실 그 너머》는 펜데믹으로 인해 이성적인 사고나 축척된 데이터가 힘을 잃어버린 상황에서 예
-술가들의 다면적인 시각과 무한한 가능성이 투영된 작품을 통해 새로운 사고의 메커니즘으로 나아가기 위해 기
-획되었다.
-</p><p>
-메타픽션(meta-fiction)은 픽션과 현실 사이의 관계에 대한 의문을 제기하기 위한 장치이다. 창작물이 허구
-(픽션)라는 것을 의도적으로 알려, 허구와 현실의 관계에 대한 문제를 제시하며 아이러니와 자아 성찰을 유도
-한다. 작품 안에서 사실과 픽션을 넘나드는 메타픽션은 작품의 내재적 존재를 탐색하기 위한 가장 좋은 장치가
-된다. 
-</p><p>
-《메타픽션: 현실 그 너머》는 ‘현실’과 ‘그 너머’의 상호작용을 경험하며 불확실한 현실을 인식하게 되는 메타
-픽션을 다루고 있다. 전시는 작품의 불확실한 이미지를 통해 현실의 인식을 유도한다. 그로 인해 현실과 픽션이
-분리되지 않고 만나게 되는 동시성을 갖는다.  
-</p><p>
-아직 펜데믹은 끝나지 않았다. 펜데믹으로 인해 불완전한 현 시점과 초현실주의는 맞닿아 있기에 초현실주의
-작가들이 이번 전시의 시발점이 된다. 익숙한 것들을 낯설게 만들어내고, 형태를 알 수 없는 작품을 통해 현실
-그 너머의 ‘초현실’을 그려낸다.  
-</p><p>
-초현실주의 시인 앙드레 브르통(Andre Breton, 1986-1966)은 화가는 내적이고도 비가시적인 꿈과 상상력을
-재현해냄으로써 현실의 억압으로부터 정신을 해방해야 된다고 선언했다. 초현실주의의 변함없는 원칙은 인간
-의 자유이다. 인간의 ‘삶’ 자체를 재정의 하면서 ‘예술 근본’의 재구성을 시도했다. 이 지점부터 전시는 시작된
-다.  
-</p><p>
-이번 전시는 마르크 샤갈(Marc Chagall, 1887-1985), 호안 미로(Joan Miro, 1893-1983), 살바도르 달리
-(Salvador Dali, 1904-1989), 쿠사마 야요이(Kusama Yayoi, b.1929), 변종곤(BYUN Chonggon, b.1948), 이
-미주(LEE Miju, b.1982), 김미진(KIM Mijin, b.1979), 쑨지(Soonji, b.1983), 이병찬(LEE Byungchan,
-b.1987) 아홉 작가의 작품 50여점을 선보인다.  
-</p><p>
-일곱 개의 픽션 ‘무의식-환영-결합-증식-우연-내재-이면’으로 현실에 대한 의문을 제기한다. 픽션의 개별 작품
-들은 자유롭게 상호 교류될 수 있도록 구성하였다. 1전시실에는 초현실주의 작가 마르크 샤갈, 호안 미로, 살바
-도르 달리를 시작으로 쿠사마 야요이, 변종곤, 이미주의 작품을, 전시실을 연결하는 공간에는 김미진의 작품으
-로, 2전시실에는 쑨지, 이병찬의 작품을 배치했다. 전시는 현실을 기반으로 하고 있지만 비현실적인 이야기를
-다루고 있기 때문에 전체적으로 현실과 초현실, 익숙함과 기괴함이 공존하고 있다. 작품들은 무의식의 산물인
-까닭에 규정짓기는 모호하지만, 그 모호함이 초현실과 기괴함을 양산한다.  
-</p><p>
-전시는 작품과 관람자를 소통시켜 예술작품에 내포된 의미를 사회화하는 기능을 한다. 《메타픽션》은 감성,
-비합리, 비논리, 우연이 힘을 발휘하는 ‘현실 그 너머’의 세계를 보여줄 것이다. 전시를 통해 현실이 가상으로,
-가상이 현실로 느껴지는 묘한 경험을 즐기며, 상상했던 기형적인 세상, 잠재의식에 내포되어 있는 인간의 삶에
-대해 의문을 제기할 수 있길 바란다.
-</p>
-</pre>
+
+
+<div class="text_wrap">
+    <h3 class="cont_tit">연혁</h3>
+</div>
+<div class="history_wrap bdTop alignC pc_ver">
+    <div class="inner">
+        <dl class="first">
+            <dt><img src="images/history_y_img01.png" alt="2006년"></dt>
+            <dd>
+                <ul>
+                    <li><span class="date">11/15</span> 미술관 건립공사 착공</li>
+                </ul>
+            </dd>
+        </dl>
+        <dl>
+            <dt><img src="images/history_y_img02.png" alt="2009년"></dt>
+            <dd>
+                <ul>
+                    <li><span class="date">11/15</span> 미술관  본관동 준공</li>
+                    <li><span class="date">12/22</span> 미술관 개관  </li>
+                </ul>
+            </dd>
+        </dl>
+        <dl>
+            <dt><img src="images/history_y_img03.png" alt="2010년"></dt>
+            <dd>
+                <ul>
+                    <li><span class="date">03/31</span> 증설수장고 준공</li>
+                </ul>
+            </dd>
+        </dl>
+    </div>
 </div>
 HTML;
 
