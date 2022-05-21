5ec93340bae94ec2794880cc0cd11a1d7ffc7ca6
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-21 19:13:54 +0900

    Update post_collection_collection.sql

--- data/post_collection_collection.sql
+++ data/post_collection_collection.sql
@@ -1,100 +1,102 @@
--- 2019년 9월 전시 도슨트 자원봉사자 전시교육
+-- [드로잉&판화] 무제
 INSERT INTO museum_post 
-(wdate, userid, nickname, subject, category, title, file, info, content) 
+(wdate, userid, nickname, subject, category, title, file, info) 
 VALUES 
-( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'education', 'edu03',
-'2019년 9월 전시 도슨트 자원봉사자 전시교육',
-'editor_176719_1565694333_0_0.png',
+( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'collection', 'collection',
+'[드로잉&판화] 무제',
+'3534906791_WJwV3oim_pd00198_B9ABC1A6.jpg',
 '{
-  "상태": "접수마감",
-  "교육대상": "미술관에서 활동중인 POMA 도슨트",
-  "교육장소": "미술관 세미나실 및 전시실",
-  "교육일자": "2019-08-20 ~ 2019-09-06",
-  "모집기간": "2019-08-13 00:00 ~ 2019-08-19 00:00",
-  "모집인원": "",
-  "참가비용": "",
-  "신청방법": ""
-}',
-'내용'
+  "작가명": "문신",
+  "작품NO": "705",
+  "분류번호": "pd00198",
+  "작품크기": "32x24",
+  "작품재료": "종이에 펜",
+  "제작년도": ""
+}'
 );
 
--- 2020년 9월 전시 도슨트 자원봉사자 전시교육
+-- [조각] 환생
 INSERT INTO museum_post 
-(wdate, userid, nickname, subject, category, title, file, info, content) 
+(wdate, userid, nickname, subject, category, title, file, info) 
 VALUES 
-( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'education', 'edu03',
-'2020년 9월 전시 도슨트 자원봉사자 전시교육',
-'editor_176719_1565694333_0_0.png',
+( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'collection', 'collection',
+'[조각] 환생',
+'3534906791_koc50BTA_IMG_5288_copy_-C5A9B1E2BAAFC8AF.jpg',
 '{
-  "상태": "접수마감",
-  "교육대상": "미술관에서 활동중인 POMA 도슨트",
-  "교육장소": "미술관 세미나실 및 전시실",
-  "교육일자": "2020-10-23 ~ 2020-10-30",
-  "모집기간": "2020-10-14 00:00 ~ 2020-10-30 00:00",
-  "모집인원": "",
-  "참가비용": "",
-  "신청방법": ""
-}',
-'내용'
+  "작가명": "이점원",
+  "작품NO": "706",
+  "분류번호": "sc00080",
+  "작품크기": "152x90x120",
+  "작품재료": "브론즈",
+  "제작년도": "1987"
+}'
 );
 
--- 2021년 10월 전시 도슨트 자원봉사자 전시교육
+-- [조각] 무제
 INSERT INTO museum_post 
-(wdate, userid, nickname, subject, category, title, file, info, content) 
+(wdate, userid, nickname, subject, category, title, file, info) 
 VALUES 
-( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'education', 'edu03',
-'2021년 10월 전시 도슨트 자원봉사자 전시교육',
-'editor_176719_1565694333_0_0.png',
+( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'collection', 'collection',
+'[조각] 무제',
+'3534906791_pWq1X3uS_IMG_0091_1_-_C5A9B1E2BAAFC8AF.jpg',
 '{
-  "상태": "접수마감",
-  "교육대상": "미술관에서 활동중인 POMA 도슨트",
-  "교육장소": "미술관 세미나실 및 전시실",
-  "교육일자": "2021-10-18 ~ 2021-10-25",
-  "모집기간": "2021-10-18 00:00 ~ 2021-10-25 00:00",
-  "모집인원": "",
-  "참가비용": "",
-  "신청방법": ""
-}',
-'내용'
+  "작가명": "홍기원",
+  "작품NO": "707",
+  "분류번호": "sc00081",
+  "작품크기": "200x200x400",
+  "작품재료": "철,산업용 저울",
+  "제작년도": "2012"
+}'
 );
 
--- 2022년 1월 전시 도슨트 자원봉사자 전시교육
+-- [조각] 이브61-3-01
 INSERT INTO museum_post 
-(wdate, userid, nickname, subject, category, title, file, info, content) 
+(wdate, userid, nickname, subject, category, title, file, info) 
 VALUES 
-( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'education', 'edu03',
-'2022년 1월 전시 도슨트 자원봉사자 전시교육',
-'editor_176719_1565694333_0_0.png',
+( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'collection', 'collection',
+'[조각] 이브61-3-01',
+'3534906791_tSx45HBE_C0CCBAEA61-328C3D6B8B8B8B0BCB1BBFDB4D429_-_C5A9B1E2BAAFC8AF.jpg',
 '{
-  "상태": "접수마감",
-  "교육대상": "미술관에서 활동중인 POMA 도슨트",
-  "교육장소": "미술관 세미나실 및 전시실",
-  "교육일자": "2022-01-10 ~ 2022-02-07",
-  "모집기간": "2021-01-10 00:00 ~ 2021-02-07 00:00",
-  "모집인원": "",
-  "참가비용": "",
-  "신청방법": ""
-}',
-'내용'
+  "작가명": "최만린",
+  "작품NO": "708",
+  "분류번호": "sc00082",
+  "작품크기": "88x86x18",
+  "작품재료": "브론즈",
+  "제작년도": "1961"
+}'
 );
 
--- 2022년 5월 전시 도슨트 자원봉사자 전시교육
+-- [서예] 포항구전민요
 INSERT INTO museum_post 
-(wdate, userid, nickname, subject, category, title, file, info, content) 
+(wdate, userid, nickname, subject, category, title, file, info) 
 VALUES 
-( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'education', 'edu03',
-'2022년 5월 전시 도슨트 자원봉사자 전시교육',
-'editor_176719_1565694333_0_0.png',
+( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'collection', 'collection',
+'[서예] 포항구전민요',
+'3534906791_lheNHRki_C5A9B1E2BAAFC8AF_BCD6B8FE_C1A4C7F6BDC4-131003-1_-_BAB9BBE7BABB.jpg',
 '{
-  "상태": "접수마감",
-  "교육대상": "미술관에서 활동중인 POMA 도슨트",
-  "교육장소": "미술관 세미나실 및 전시실",
-  "교육일자": "2022-05-02 ~ 2022-06-07",
-  "모집기간": "2021-05-02 00:00 ~ 2021-06-07 00:00",
-  "모집인원": "",
-  "참가비용": "",
-  "신청방법": ""
-}',
-'내용'
+  "작가명": "정현식",
+  "작품NO": "709",
+  "분류번호": "ca00001",
+  "작품크기": "135x70(4폭 병풍)",
+  "작품재료": "화선지에 먹",
+  "제작년도": "2013"
+}'
+);
+
+-- [회화] 무제
+INSERT INTO museum_post 
+(wdate, userid, nickname, subject, category, title, file, info) 
+VALUES 
+( UNIX_TIMESTAMP(), 'admin', '포항시립미술관', 'collection', 'collection',
+'[회화] 무제',
+'3534906791_tIwF5drG_C1A4C1A1BDC4_C0DB_B9ABC1A6-1_65x91cm_-_C5A9B1E2BAAFC8AF.jpg',
+'{
+  "작가명": "정점식",
+  "작품NO": "710",
+  "분류번호": "pa00422",
+  "작품크기": "65x91",
+  "작품재료": "캔버스에 유채",
+  "제작년도": "1989"
+}'
 );
 
