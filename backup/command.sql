555c0854b211bb75ed0dfd50f992de907ee82cf1
LeeGyuho <andrewgyuholee@gmail.com>
2022-05-22 00:15:48 +0900

    Update command.sql

--- backup/command.sql
+++ backup/command.sql
@@ -1,5 +1,5 @@
 # 데이터베이스 전부 백업
-$ mysqldump -u root museum > museum_20220521.dump.sql
+$ mysqldump -u root museum > museum_20220522.dump.sql
 
 # 데이터베이스 전부 리스토어
-$ mysql -u root museum < museum_20220521.dump.sql
+$ mysql -u root museum < museum_20220522.dump.sql
